5026a39ffac447982ceceb95ee520674eab4f853
datoprak <106121887+datoprak@users.noreply.github.com>
2023-03-14 17:10:57 +0300

    Create basic style and early stage of gameboard function

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert("hello");
+const Gameboard = (() => {
+  const gameboard = ["x", "o", "x", "x", "o", "x", "x", "o", "x"];
+  const squares = document.querySelectorAll(".square");
+  squares.forEach((square, index) => {
+    square.textContent = gameboard[index];
+  });
+})();
+
+const Player = name => {
+  const getName = () => name;
+};
+
+const Game = () => {};
