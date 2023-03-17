999f0467284b1a61e39b48f8a4a5fcc8bb967a8d
datoprak <106121887+datoprak@users.noreply.github.com>
2023-03-17 18:08:32 +0300

    Fix gameboard function and create controller and game functions

--- script.js
+++ script.js
@@ -1,13 +1,72 @@
 const Gameboard = (() => {
-  const gameboard = ["x", "o", "x", "x", "o", "x", "x", "o", "x"];
-  const squares = document.querySelectorAll(".square");
-  squares.forEach((square, index) => {
-    square.textContent = gameboard[index];
-  });
+  const createBoard = (() => {
+    const gameboardDiv = document.querySelector(".gameboard");
+    const gameboard = [];
+    for (let i = 0; i < 3; i++) {
+      gameboard[i] = [];
+      for (let j = 0; j < 3; j++) {
+        gameboard[i].push("");
+        const button = document.createElement("button");
+        button.classList.add("square");
+        button.dataset.row = i;
+        button.dataset.column = j;
+        gameboardDiv.appendChild(button);
+      }
+    }
+
+    return { gameboard };
+  })();
+
+  const getBoard = () => {
+    const buttons = document.querySelectorAll(".square");
+    buttons.forEach(button => {
+      button.textContent =
+        createBoard.gameboard[button.dataset.row][button.dataset.column];
+    });
+  };
+
+  return { getBoard, createBoard };
 })();
 
-const Player = name => {
-  const getName = () => name;
-};
+const Controller = (() => {
+  let activePlayer = "playerOne";
 
-const Game = () => {};
+  const getPlayerMark = () => (activePlayer === "playerOne" ? "X" : "O");
+
+  const switchPlayer = () => {
+    activePlayer = activePlayer === "playerOne" ? "playerTwo" : "playerOne";
+  };
+
+  const startNewRound = () => {
+    Gameboard.getBoard();
+  };
+
+  const playRound = () => {
+    startNewRound();
+    // check for winner
+
+    switchPlayer();
+  };
+
+  const addMark = e => {
+    const row = e.target.dataset.row;
+    const column = e.target.dataset.column;
+
+    if (
+      e.target.value === undefined ||
+      Gameboard.createBoard.gameboard[row][column] !== ""
+    )
+      return;
+
+    Gameboard.createBoard.gameboard[row][column] = getPlayerMark();
+    playRound();
+  };
+
+  return { addMark };
+})();
+
+const Game = (() => {
+  Gameboard.createBoard;
+  const gameboardDiv = document.querySelector(".gameboard");
+  gameboardDiv.addEventListener("click", Controller.addMark);
+})();
